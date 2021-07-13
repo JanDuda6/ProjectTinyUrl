b5300d5cffd9a68288f4fdcafe3a12621de7e662
Jan Duda <jan.duda6@gmail.com>
2021-07-13 16:17:43 +0200

    networking done

---
+++ Project Tiny Url/Project Tiny Url/Model/LongURLToSend.swift
@@ -0,0 +1,18 @@
+//
+//  LongURLToSend.swift
+//  Project Tiny Url
+//
+//  Created by Kurs on 13/07/2021.
+//
+
+import Foundation
+
+struct LongURLToSend: Codable {
+    var format: String
+    var url: String
+
+    init(url: String) {
+        self.url = url
+        self.format = "json"
+    }
+}
