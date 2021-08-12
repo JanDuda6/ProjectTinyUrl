d2ce1b04391d20c8db07a957579ec97b38c1e887
Jan Duda <jan.duda6@gmail.com>
2021-08-12 09:44:23 +0200

    translations added, set up tableView

---
+++ Project Tiny Url/Translations.swift
@@ -0,0 +1,15 @@
+//
+//  Translation.swift
+//  Project Tiny Url
+//
+//  Created by Kurs on 12/08/2021.
+//
+
+import Foundation
+
+class Translations {
+    static var labelText = NSLocalizedString("Enter URL:", comment: "")
+    static var buttonText = NSLocalizedString("Make it Tiny!", comment: "")
+    static var alertTitle = NSLocalizedString("Success!", comment: "")
+    static var alertMessage = NSLocalizedString("Tiny URL copied to clipboard!", comment: "")
+}
